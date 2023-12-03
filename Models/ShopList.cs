8fa1633e30a038450b78b8ef01ca243153ac258c
Andreea Socol <andreeas.2002@yahoo.com>
2023-12-04 00:18:01 +0200

    lab8

---
+++ Models/ShopList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SQLite;
+using System.Threading.Tasks;
+
+namespace Socol_Andreea_Lab7.Models
+{
+    public class ShopList
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+
+        [MaxLength(250), Unique]
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
